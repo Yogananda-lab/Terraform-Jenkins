b196a279804a366b2704106acb936fb1154263ed
Yogananda-lab <71587868+Yogananda-lab@users.noreply.github.com>
2021-06-21 11:49:10 +0530

    Create Provider.tf

---
+++ Provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "${var.region}"
+
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "jenkins-tf-1"
+    key    = "terraform.tfstate"
+    region =  "us-east-2"
+  }
+}
